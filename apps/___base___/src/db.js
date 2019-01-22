17f357a738770a1bf66914777fe2839504a805cc
Ty Barho <t.barho@gmail.com>
2019-01-22 15:42:33 -0600

    cleanup

--- apps/___base___/src/db.js
+++ apps/___base___/src/db.js
@@ -3,7 +3,7 @@ import { startSequelize } from '../../../utils/sequelize/startSequelize';
 
 
 const startDB = async () => {
-  const db = await startSequelize('___BASE___', Models);
+  const db = await startSequelize('___BASE____', Models);
   return db;
 }
 
