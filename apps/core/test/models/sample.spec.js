dcac71d3b82a388a6dc2fac518843034506ce62b
Ty Barho <t.barho@gmail.com>
2019-01-22 15:17:42 -0600

    sample model spec

---
+++ apps/core/test/models/sample.spec.js
@@ -0,0 +1,20 @@
+describe('Sample - Model', () => {
+
+  it('should create', async () => {
+    const record = await apps.CoreApp.factory.create('Sample', { name: 'New Sample' });
+    expect((await apps.CoreApp.db.Sample.findByPk(record.id)).name).to.equal('New Sample');
+  });
+
+  describe('Validations', () => {
+
+  });
+
+  describe('Associations', () => {
+
+  });
+
+  describe('Lifecycle', () => {
+
+  });
+
+});
